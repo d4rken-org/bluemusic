77bf9abb52587d27aea343c28a4d4f9c4f5b3850
darken <darken@darken.eu>
2017-08-30 21:01:13 +0200

    Move to new version format 2.XX

--- app/src/mock/java/eu/darken/bluemusic/core/bluetooth/FakeDeviceSource.java
+++ app/src/mock/java/eu/darken/bluemusic/core/bluetooth/FakeDeviceSource.java
@@ -1,94 +1,32 @@
 package eu.darken.bluemusic.core.bluetooth;
 
-import android.bluetooth.BluetoothDevice;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Locale;
-import java.util.Random;
-import java.util.UUID;
+import java.util.Map;
 
-import static eu.darken.bluemusic.core.bluetooth.BluetoothEventReceiver.EXTRA_DEVICE_ADDRESS;
+import io.reactivex.Single;
 
 public class FakeDeviceSource implements BluetoothSource {
 
     private final Context context;
-    private final Handler handler;
-    private final Collection<Device> devices = new ArrayList<>();
 
     public FakeDeviceSource(Context context) {
         this.context = context;
-        HandlerThread thread = new HandlerThread("FakeDeviceSourceThread");
-        thread.start();
-        handler = new Handler(thread.getLooper());
-        for (int i = 0; i < 10; i++) {
-            final String name = "Device " + i;
-            final String address = UUID.randomUUID().toString();
-            final Device device = new Device() {
-                @Nullable
-                @Override
-                public String getName() {
-                    return name;
-                }
-
-                @NonNull
-                @Override
-                public String getAddress() {
-                    return address;
-                }
-
-                @Override
-                public String toString() {
-                    return String.format(Locale.US, "Device(name=%s, address=%s)", getName(), getAddress());
-                }
-            };
-            devices.add(device);
-            handler.postDelayed(makeDeviceConnected(device), randTime());
-        }
-    }
-
-    private static long randTime() {
-        long lower = 3 * 1000; //assign lower range value
-        long upper = 30 * 1000; //assign upper range value
-        Random random = new Random();
-
-
-        return lower + (long) (random.nextDouble() * (upper - lower));
-    }
-
-    private Runnable makeDeviceConnected(Device device) {
-        return () -> {
-            Intent intent = new Intent();
-            intent.setAction(BluetoothDevice.ACTION_ACL_CONNECTED);
-            intent.putExtra(EXTRA_DEVICE_ADDRESS, device.getAddress());
-            new BluetoothEventReceiver().onReceive(context, intent);
-            handler.postDelayed(makeDeviceDisconnected(device), randTime());
-        };
-    }
-
-    private Runnable makeDeviceDisconnected(Device device) {
-        return () -> {
-            Intent intent = new Intent();
-            intent.setAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-            intent.putExtra(EXTRA_DEVICE_ADDRESS, device.getAddress());
-            new BluetoothEventReceiver().onReceive(context, intent);
-            handler.postDelayed(makeDeviceConnected(device), randTime());
-        };
     }
 
     @Override
-    public Collection<Device> getPairedDevices() {
-        return devices;
+    public Single<Map<String, SourceDevice>> getPairedDevices() {
+        return Single.error(new UnsupportedOperationException());
     }
 
     @Override
-    public Collection<Device> getConnectedDevices(int profile) {
-        return devices;
+    public Single<Map<String, SourceDevice>> getConnectedDevices() {
+        return Single.error(new UnsupportedOperationException());
+    }
+
+    @Override
+    public boolean isEnabled() {
+        throw new UnsupportedOperationException();
+//        return false;
     }
 }
