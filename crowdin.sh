9cd731e6fd11c1998490c26f4ca7760e7cca739a
darken <darken@darken.eu>
2018-10-14 01:05:30 +0200

    Tooling updates

--- crowdin.sh
+++ crowdin.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 export CROWDIN_API_KEY=`cat ~/.appconfig/eu.darken.bluemusic/crowdin.key`
-alias crowdin='crowdin-cli'
+alias crowdin-cli='java -jar /usr/local/bin/crowdin-cli.jar'
 crowdin-cli "$@"
