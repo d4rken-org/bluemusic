7fad6f76f75abc3f03e19166e40d7c7ceff37d12
darken <darken@darken.eu>
2019-02-15 19:34:11 +0100

    Improved start/stop behavior (startForeground issues) v2

--- app/src/main/java/eu/darken/bluemusic/main/core/service/ServiceHelper.java
+++ app/src/main/java/eu/darken/bluemusic/main/core/service/ServiceHelper.java
@@ -1,9 +1,9 @@
 package eu.darken.bluemusic.main.core.service;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +12,7 @@ import android.support.v4.app.NotificationCompat;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -19,6 +20,15 @@ import eu.darken.bluemusic.R;
 import eu.darken.bluemusic.ResHelper;
 import eu.darken.bluemusic.main.core.database.ManagedDevice;
 import eu.darken.bluemusic.main.ui.MainActivity;
+import eu.darken.bluemusic.util.ValueBox;
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.Disposables;
+import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
 
 @BlueMusicServiceComponent.Scope
@@ -30,13 +40,14 @@ public class ServiceHelper {
     private final NotificationManager notificationManager;
     private final ResHelper resHelper;
     private final NotificationCompat.Builder builder;
-    private final Service service;
-    private boolean started;
-    private long startCall = 0;
+    private ObservableEmitter<String> emitter;
+    private volatile Disposable serviceStopper = Disposables.disposed();
+    private volatile boolean isStarted = false;
+    private static final String CMD_START = "start";
+    private static final String CMD_STOP = "stop";
 
     @Inject
     ServiceHelper(BlueMusicService service, NotificationManager notificationManager, ResHelper resHelper) {
-        this.service = service;
         this.notificationManager = notificationManager;
         this.resHelper = resHelper;
 
@@ -60,30 +71,53 @@ public class ServiceHelper {
                 .setContentTitle(resHelper.getString(R.string.app_name))
                 .addAction(new NotificationCompat.Action.Builder(0, service.getString(R.string.action_exit), stopPi).build());
 
+        final ValueBox<String> lastCmd = new ValueBox<>();
+        Observable.create((ObservableOnSubscribe<String>) emitter -> ServiceHelper.this.emitter = emitter)
+                .doOnNext(cmd -> Timber.v("Submitted: cmd-%s", cmd))
+                .filter(cmd -> !cmd.equals(lastCmd.getValue()))
+                .doOnNext(lastCmd::setValue)
+                .subscribe(cmd -> {
+                    Timber.d("Processing cmd-%s", cmd);
+                    if (cmd.equals(CMD_START)) {
+                        if (!serviceStopper.isDisposed()) {
+                            Timber.d("Stopping on-going shutdown due to cmd-%s", cmd);
+                            serviceStopper.dispose();
+                        }
+
+                        if (isStarted) {
+                            Timber.d("Ignoring cmd-%s, already started!", cmd);
+                            return;
+                        }
+
+                        isStarted = true;
+                        Timber.d("Executing startForeground()");
+                        service.startForeground(NOTIFICATION_ID, builder.build());
+                    } else {
+                        if (!isStarted) {
+                            Timber.w("Calling stopForeground() without startForeground()");
+                            emitter.onNext(CMD_START);
+                            emitter.onNext(CMD_STOP);
+                            return;
+                        }
+                        serviceStopper = Completable.timer(1500, TimeUnit.MILLISECONDS)
+                                .subscribeOn(Schedulers.io())
+                                .doOnComplete(() -> isStarted = false)
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(() -> {
+                                    Timber.d("Executing stopForeground()");
+                                    service.stopForeground(true);
+                                    service.stopSelf();
+                                });
+                    }
+                });
     }
 
-    synchronized void start() {
-        Timber.d("start(started=%b, time=%d, thread=%s)", started, (startCall > 0 ? System.currentTimeMillis() - startCall : 0), Thread.currentThread());
-        if (started) {
-            Timber.d("Service already launched to foreground.");
-            return;
-        }
-        started = true;
-        service.startForeground(NOTIFICATION_ID, builder.build());
-        if (startCall == 0) startCall = System.currentTimeMillis();
+    void start() {
+        emitter.onNext(CMD_START);
     }
 
-    synchronized void stop() {
-        Timber.d("stop(started=%b, time=%d, thread=%s)", started, (startCall > 0 ? System.currentTimeMillis() - startCall : 0), Thread.currentThread());
-        if (!started) {
-            // Context.startForegroundService() did not then call Service.startForeground
-            // Even if we are below the ANR limit, keep the contract!
-            start();
-        }
-        started = false;
-        startCall = 0;
-        service.stopForeground(true);
-        service.stopSelf();
+    void stop() {
+        emitter.onNext(CMD_STOP);
     }
 
     public static Intent getIntent(Context context) {
@@ -98,13 +132,15 @@ public class ServiceHelper {
         }
     }
 
-    private synchronized void updateNotification() {
-        if (!started) return;
-        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    private void updateNotification() {
+        if (!isStarted) return;
+        Notification notification = builder.build();
+        Timber.v("updatingNotification()");
+        notificationManager.notify(NOTIFICATION_ID, notification);
     }
 
     void updateActiveDevices(Collection<ManagedDevice> devices) {
-        Timber.v("updateActiveDevices(devices=%s)", devices);
+        Timber.d("updateActiveDevices(devices=%s)", devices);
         final Iterator<ManagedDevice> iterator = devices.iterator();
         StringBuilder sb = new StringBuilder();
         while (iterator.hasNext()) {
@@ -117,7 +153,7 @@ public class ServiceHelper {
     }
 
     void updateMessage(String message) {
-        Timber.v("updateMessage(message=%s)", message);
+        Timber.d("updateMessage(message=%s)", message);
         builder.setContentText(message);
         updateNotification();
     }
