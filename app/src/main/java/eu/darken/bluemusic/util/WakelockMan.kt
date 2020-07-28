cec289bd22b370ede8e538b9432add654aeb1e14
darken <darken@darken.eu>
2020-07-28 09:47:13 +0200

    Catch and log exceptions when acquiring or releasing wakelocks. Some devices throw SecurityExceptions

--- app/src/main/java/eu/darken/bluemusic/util/WakelockMan.kt
+++ app/src/main/java/eu/darken/bluemusic/util/WakelockMan.kt
@@ -1,6 +1,7 @@
 package eu.darken.bluemusic.util
 
 import android.os.PowerManager
+import com.bugsnag.android.Bugsnag
 import eu.darken.bluemusic.AppComponent
 import timber.log.Timber
 import javax.inject.Inject
@@ -9,21 +10,34 @@ import javax.inject.Inject
 class WakelockMan @Inject constructor(powerManager: PowerManager) {
 
     @Suppress("DEPRECATION")
-    private val wakelock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "bvm:keepawake")
+    private val wakelock = powerManager.newWakeLock(
+            PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+            "bvm:keepawake"
+    )
 
     fun tryAquire() {
         if (wakelock.isHeld) {
             Timber.d("tryAquire(): Wakelock already held.")
             return
         } else {
-            wakelock.acquire(3 * 60 * 60 * 1000)
-            Timber.d("tryAquire(): Wakelock acquired (isHeld=%b)", wakelock.isHeld)
+            try {
+                wakelock.acquire(3 * 60 * 60 * 1000)
+                Timber.d("tryAquire(): Wakelock acquired (isHeld=%b)", wakelock.isHeld)
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to acquire wakelock.")
+                Bugsnag.notify(e)
+            }
         }
     }
 
     fun tryRelease() {
         if (wakelock.isHeld) {
-            wakelock.release()
+            try {
+                wakelock.release()
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to release wakelock.")
+                Bugsnag.notify(e)
+            }
             Timber.d("tryRelease(): Wakelock released (isHeld=%b)", wakelock.isHeld)
         } else {
             Timber.d("tryRelease(): Wakelock is not acquired.")
