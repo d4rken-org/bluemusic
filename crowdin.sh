64094f3367446ed67b167a8cac0aa207f3616d75
darken <darken@darken.eu>
2022-02-15 18:18:21 +0100

    Fix crowdin translation script

--- crowdin.sh
+++ crowdin.sh
@@ -1,3 +1,3 @@
 export CROWDIN_API_KEY=`cat ~/.appconfig/eu.darken.bluemusic/crowdin.key`
-alias crowdin-cli='java -jar /usr/local/bin/crowdin-cli.jar'
+alias crowdin-cli='java -jar crowdin-cli.jar'
 crowdin-cli "$@"
