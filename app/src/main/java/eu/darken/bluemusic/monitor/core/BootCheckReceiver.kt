ef2474a12acc1acc362ff1d610c7459742db269e
darken <darken@darken.eu>
2025-07-23 16:49:00 +0200

    wip

--- app/src/main/java/eu/darken/bluemusic/monitor/core/BootCheckReceiver.kt
+++ app/src/main/java/eu/darken/bluemusic/monitor/core/BootCheckReceiver.kt
@@ -5,11 +5,12 @@ import android.content.Context
 import android.content.Intent
 import dagger.hilt.android.AndroidEntryPoint
 import eu.darken.bluemusic.bluetooth.core.BluetoothRepo
-import eu.darken.bluemusic.bluetooth.core.SourceDevice
 import eu.darken.bluemusic.bluetooth.core.currentState
 import eu.darken.bluemusic.common.coroutine.DispatcherProvider
 import eu.darken.bluemusic.common.datastore.valueBlocking
-import eu.darken.bluemusic.common.debug.logging.Logging
+import eu.darken.bluemusic.common.debug.logging.Logging.Priority.ERROR
+import eu.darken.bluemusic.common.debug.logging.Logging.Priority.INFO
+import eu.darken.bluemusic.common.debug.logging.Logging.Priority.VERBOSE
 import eu.darken.bluemusic.common.debug.logging.asLog
 import eu.darken.bluemusic.common.debug.logging.log
 import eu.darken.bluemusic.common.debug.logging.logTag
@@ -34,18 +35,18 @@ class BootCheckReceiver : BroadcastReceiver() {
     @Inject lateinit var monitorControl: MonitorControl
 
     override fun onReceive(context: Context, intent: Intent) {
-        log(TAG, Logging.Priority.VERBOSE) { "onReceive($context, $intent)" }
+        log(TAG, VERBOSE) { "onReceive($context, $intent)" }
         if (Intent.ACTION_BOOT_COMPLETED != intent.action) {
-            log(TAG, Logging.Priority.ERROR) { "Triggered with unknown intent: $intent" }
+            log(TAG, ERROR) { "Triggered with unknown intent: $intent" }
             return
         }
 
         if (!devicesSettings.isEnabled.valueBlocking) {
-            log(TAG, Logging.Priority.INFO) { "We are disabled." }
+            log(TAG, INFO) { "We are disabled." }
             return
         }
         if (!devicesSettings.restoreOnBoot.valueBlocking) {
-            log(TAG, Logging.Priority.INFO) { "Restoring on boot is disabled." }
+            log(TAG, INFO) { "Restoring on boot is disabled." }
             return
         }
 
@@ -65,31 +66,17 @@ class BootCheckReceiver : BroadcastReceiver() {
                     return@launch
                 }
 
-                val connectedDevices = state.devices!!
-                if (connectedDevices.isEmpty()) {
-                    log(TAG) { "No devices were connected on boot." }
-                    return@launch
-                }
-
+                val connectedDevices = deviceRepo.devices.first().filter { it.isActive }
                 log(TAG) { "We booted with already connected devices: $connectedDevices" }
 
-                val managedConnectedDevices = mutableListOf<SourceDevice>()
-                val managedDevices = deviceRepo.devices.first()
-
-                connectedDevices.forEach { device ->
-                    if (managedDevices.any { it.address == device.address }) {
-                        managedConnectedDevices.add(device)
-                    }
-                }
-
-                if (managedConnectedDevices.isEmpty()) {
-                    log(TAG, Logging.Priority.INFO) { "Connected devices are not managed by us." }
+                if (connectedDevices.isEmpty()) {
+                    log(TAG, INFO) { "Connected devices are not managed by us." }
                     return@launch
                 }
 
                 monitorControl.startMonitor()
             } catch (e: Exception) {
-                log(TAG, Logging.Priority.ERROR) { "Error during boot check: ${e.asLog()}" }
+                log(TAG, ERROR) { "Error during boot check: ${e.asLog()}" }
             } finally {
                 pendingResult.finish()
                 scope.cancel()
