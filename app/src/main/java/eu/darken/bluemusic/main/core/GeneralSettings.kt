f5ff3f3ee6ed21e8f5d8c5cb833e86db1b576e78
darken <darken@darken.eu>
2026-03-02 02:20:27 +0100

    Add deserialization error fallback to DataStore and fix BillingCache duplicate key

--- app/src/main/java/eu/darken/bluemusic/main/core/GeneralSettings.kt
+++ app/src/main/java/eu/darken/bluemusic/main/core/GeneralSettings.kt
@@ -5,6 +5,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
+import eu.darken.bluemusic.common.BuildConfigWrap
 import eu.darken.bluemusic.common.datastore.PreferenceData
 import eu.darken.bluemusic.common.datastore.createValue
 import eu.darken.bluemusic.common.debug.logging.logTag
@@ -25,8 +26,14 @@ class GeneralSettings @Inject constructor(
     override val dataStore: DataStore<Preferences>
         get() = context.dataStore
 
-    val themeMode = dataStore.createValue("core.ui.theme.mode", ThemeMode.SYSTEM, json)
-    val themeStyle = dataStore.createValue("core.ui.theme.style", ThemeStyle.DEFAULT, json)
+    val themeMode = dataStore.createValue(
+        "core.ui.theme.mode", ThemeMode.SYSTEM, json,
+        onErrorFallbackToDefault = BuildConfigWrap.BUILD_TYPE == BuildConfigWrap.BuildType.RELEASE,
+    )
+    val themeStyle = dataStore.createValue(
+        "core.ui.theme.style", ThemeStyle.DEFAULT, json,
+        onErrorFallbackToDefault = BuildConfigWrap.BUILD_TYPE == BuildConfigWrap.BuildType.RELEASE,
+    )
 
     val isOnboardingCompleted = dataStore.createValue("core.onboarding.completed", false)
 
