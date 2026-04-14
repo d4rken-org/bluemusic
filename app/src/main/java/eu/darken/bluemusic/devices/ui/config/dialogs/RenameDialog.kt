f3ed791261f9e570fde9988ba94c73858b0487ed
darken <darken@darken.eu>
2026-04-14 07:15:13 +0200

    Add keyboard done action to rename dialog

--- app/src/main/java/eu/darken/bluemusic/devices/ui/config/dialogs/RenameDialog.kt
+++ app/src/main/java/eu/darken/bluemusic/devices/ui/config/dialogs/RenameDialog.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -16,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import eu.darken.bluemusic.R
@@ -38,6 +41,13 @@ fun RenameDialog(
                     value = name,
                     onValueChange = { name = it },
                     singleLine = true,
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    keyboardActions = KeyboardActions(onDone = {
+                        if (name.isNotBlank()) {
+                            onConfirm(name)
+                            onDismiss()
+                        }
+                    }),
                     modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(modifier = Modifier.height(16.dp))
