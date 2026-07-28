c825cd492cfc43dbceaa9f3c681e4a18072a1265
darken <darken@darken.eu>
2026-07-28 14:23:10 +0200

    Add tests for foreground promotion on every start command

---
+++ app/src/test/java/eu/darken/bluemusic/monitor/ui/MonitorNotificationsTest.kt
@@ -0,0 +1,41 @@
+package eu.darken.bluemusic.monitor.ui
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldNotBeEmpty
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE, sdk = [34])
+class MonitorNotificationsTest {
+
+    private val context: Context = ApplicationProvider.getApplicationContext()
+
+    @Test
+    fun `early notification is user presentable`() {
+        val notification = MonitorNotifications.createEarlyNotification(context)
+
+        notification.extras.getString(Notification.EXTRA_TITLE)!!.shouldNotBeEmpty()
+        notification.extras.getString(Notification.EXTRA_TEXT)!!.shouldNotBeEmpty()
+        notification.smallIcon.shouldNotBeNull()
+        notification.contentIntent.shouldNotBeNull()
+    }
+
+    @Test
+    fun `early notification ensures the status channel exists`() {
+        MonitorNotifications.createEarlyNotification(context)
+
+        val nm = context.getSystemService(NotificationManager::class.java)
+        val channel = nm.getNotificationChannel("notification.channel.core")
+
+        channel.shouldNotBeNull()
+        channel.importance shouldBe NotificationManager.IMPORTANCE_MIN
+    }
+}
